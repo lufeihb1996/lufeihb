54a0de50b16883de05bd6fc3c7ce3de80a917841
xiangrong <xiangrong@sinosoft.com.cn>
2025-06-05 11:15:34 +0800

    https://github.com/lufeihb1996/lufeihb.git

---
+++ demo/mydemo/src/App.vue
@@ -0,0 +1,68 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  111111
+ <span>{{ publishedBooksMessage }}</span>
+</template>
+
+
+
+<script>
+export default {
+  data() {
+    return {
+      author: {
+        name: 'John Doe',
+        books: [
+          'Vue 2 - Advanced Guide',
+          'Vue 3 - Basic Guide',
+          'Vue 4 - The Mystery'
+        ]
+      }
+    }
+  },
+  computed: {
+    // 一个计算属性的 getter
+    publishedBooksMessage() {
+      // `this` 指向当前组件实例
+      return this.author.books.length > 0 ? 'Yes' : 'No'
+    }
+  }
+}
+</script>
+
+
+
+
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
