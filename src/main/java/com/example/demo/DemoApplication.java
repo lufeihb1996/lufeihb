7901b39a9de1688ce5a7b80f586f776918a73beb
lufeihb1996 <lufeihb1996@gmail.com>
2025-06-05 10:47:23 +0800

    Add upload endpoint and test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,10 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @SpringBootApplication
 @RestController
@@ -16,5 +19,10 @@ public class DemoApplication {
     @GetMapping("/")
     public String home() {
         return "Hello, Spring Boot!";
+    }
+
+    @PostMapping("/upload")
+    public String upload(@RequestParam("file") MultipartFile file) {
+        return "Uploaded: " + file.getOriginalFilename();
     }
 }
